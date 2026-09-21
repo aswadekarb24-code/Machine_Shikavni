5b61093689d4ec1184ee0b96fa909853124e00be
Once-1296 <awwab.wadekar@gmail.com>
2026-09-22 02:21:30 +0530

    added custom noise funcs

--- src/evalnoise.py
+++ src/evalnoise.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import torch
 
-from config import CHECKPOINTS
+from src.config import CHECKPOINT
 from src.models.cnn import TrialCNN
 from src.models.helper import get_criterion
 from src.noises.corruptions import CORRUPTIONS
@@ -14,9 +14,9 @@ corruptions = list(CORRUPTIONS.keys())
 severities = [1,2,3,4,5]
 dsets = ['GTSRB','CTSD','BTSD']
 dsetmp = {
-    'GTSRB' : os.path.join(CHECKPOINTS, 'gtsrb_mixed.pth'),
-    'CTSD' : os.path.join(CHECKPOINTS, 'ctsd_mixed.pth'),
-    'BTSD' : os.path.join(CHECKPOINTS, 'btsd_mixed.pth'),
+    'GTSRB' : os.path.join(CHECKPOINT, 'gtsrb_mixed.pth'),
+    'CTSD' : os.path.join(CHECKPOINT, 'ctsd_mixed.pth'),
+    'BTSD' : os.path.join(CHECKPOINT, 'btsd_mixed.pth'),
 }
 
 dset_n_classes = {
